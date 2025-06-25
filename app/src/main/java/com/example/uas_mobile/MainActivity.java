c6884160eaff760512989bee8a5541c4d84a957e
WulanVini <wulanviniaprilia@gmail.com>
2025-06-25 14:14:43 +0800

    Login,regis, welcome

--- app/src/main/java/com/example/uas_mobile/MainActivity.java
+++ app/src/main/java/com/example/uas_mobile/MainActivity.java
@@ -1,70 +1,20 @@
 package com.example.uas_mobile;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
-    EditText etUsername, etPassword;
-    Button btnLogin;
-    TextView tvRegister;
-
-    ApiService apiService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        etUsername = findViewById(R.id.etUsername);
-        etPassword = findViewById(R.id.etPassword);
-        btnLogin = findViewById(R.id.btnLogin);
-        tvRegister = findViewById(R.id.tvRegister);
-
-        apiService = ApiClient.getClient().create(ApiService.class);
-
-        btnLogin.setOnClickListener(v -> {
-            String username = etUsername.getText().toString();
-            String password = etPassword.getText().toString();
-
-            if (username.isEmpty() || password.isEmpty()) {
-                Toast.makeText(this, "Isi semua field!", Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-            Call<ResponseModel> call = apiService.login(username, password);
-            call.enqueue(new Callback<ResponseModel>() {
-                @Override
-                public void onResponse(Call<ResponseModel> call, Response<ResponseModel> response) {
-                    if (response.body() != null && response.body().isSuccess()) {
-                        Toast.makeText(MainActivity.this, response.body().getMessage(), Toast.LENGTH_SHORT).show();
-                        // Arahkan ke home/dashboard jika perlu
-                    } else {
-                        Toast.makeText(MainActivity.this, response.body().getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                }
-
-                @Override
-                public void onFailure(Call<ResponseModel> call, Throwable t) {
-                    Toast.makeText(MainActivity.this, "Gagal: " + t.getMessage(), Toast.LENGTH_LONG).show();
-                }
-            });
-        });
-
-        tvRegister.setOnClickListener(v -> {
-            startActivity(new Intent(MainActivity.this, Register.class));
-            finish();
-        });
     }
 
+    public void btnMulai (View view) {
+        Intent intent = new Intent(MainActivity.this, login.class);
+        startActivity(intent);
+    }
 }
---
+++ app/src/main/java/com/example/uas_mobile/login.java
@@ -0,0 +1,70 @@
+package com.example.uas_mobile;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class login extends AppCompatActivity {
+
+    EditText etUsername, etPassword;
+    Button btnLogin;
+    TextView tvRegister;
+
+    ApiService apiService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        tvRegister = findViewById(R.id.tvRegister);
+
+        apiService = ApiClient.getClient().create(ApiService.class);
+
+        btnLogin.setOnClickListener(v -> {
+            String username = etUsername.getText().toString();
+            String password = etPassword.getText().toString();
+
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Isi semua field!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Call<ResponseModel> call = apiService.login(username, password);
+            call.enqueue(new Callback<ResponseModel>() {
+                @Override
+                public void onResponse(Call<ResponseModel> call, Response<ResponseModel> response) {
+                    if (response.body() != null && response.body().isSuccess()) {
+                        Toast.makeText(login.this, response.body().getMessage(), Toast.LENGTH_SHORT).show();
+                        // Arahkan ke home/dashboard jika perlu
+                    } else {
+                        Toast.makeText(login.this, response.body().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<ResponseModel> call, Throwable t) {
+                    Toast.makeText(login.this, "Gagal: " + t.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+        });
+
+        tvRegister.setOnClickListener(v -> {
+            startActivity(new Intent(login
+                    .this, Register.class));
+            finish();
+        });
+    }
+
+}
